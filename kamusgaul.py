febe98bf244e45c7ce5e71fec39b264388d272dd
themacaroniman <137882948+themacaroniman@users.noreply.github.com>
2023-06-27 16:49:53 +0700

    Create kamusgaul.py

---
+++ kamusgaul.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "BTW": "By the way, omong omong",
+            "CREEPY": "Menakutkan"
+            }
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("KATA TERSEBUT TIDAK ADA!")
